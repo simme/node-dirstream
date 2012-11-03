2b56efcb07955caeb875227437a537ef32e7da65
Simon Ljungberg <hi@iamsim.me>
2012-11-03 18:56:40 +0100

    Write testes

---
+++ test/dirstream.test.js
@@ -0,0 +1,45 @@
+//
+// ## Test DirStream
+//
+var assert = require('assert');
+var dirstream = require('../dirstream').DirStream;
+var path = __dirname + '/foo';
+
+suite('DirStream', function () {
+
+  test('Count is correct', function (done) {
+    var d = new dirstream(path);
+    d.on('count', function (count) {
+      assert(count, 5, 'Got count wrong.');
+      done();
+    });
+  });
+
+  test('Count works after filter', function (done) {
+    var d = new dirstream(path, /.txt$/);
+    d.on('count', function (count) {
+      assert(count, 3, 'Filter not applied correctly.');
+      done();
+    });
+  });
+
+  test('Pause/resume works.', function (done) {
+    var d = new dirstream(path);
+    var count = 0;
+    d.on('data', function (file) {
+      count++;
+      if (count == 3) {
+        d.pause();
+        setTimeout(function () {
+          assert(count, 3, 'Pause does not work.');
+          d.resume();
+        }, 20);
+      }
+    });
+    d.on('end', function () {
+      done();
+    });
+  });
+
+});
+
